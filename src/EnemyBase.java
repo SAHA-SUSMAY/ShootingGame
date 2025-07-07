f339587c3038aedeab6442f209fdbacaf635fa7e
LAPTOP-PTD4L1GV\susma <susmaysaha4800@gmail.com>
2025-07-07 10:34:36 +0900

    Lesson 12

--- src/EnemyBase.java
+++ src/EnemyBase.java
@@ -1,7 +1,17 @@
 
 public class EnemyBase extends Enemy {
+	//コンストラクタ、他のメソッドは省略
+	public void draw(MyFrame f) {
+		f.setColor(0, 128, 0);
+		f.fillOval(x, y, 32, 32);
+		f.setColor(200, 200, 200);
+		f.fillOval(x - 16, y + 8, 64, 16);
+	}
+
 	public EnemyBase(double x, double y, double vx, double vy) {
 		super(x, y, vx, vy);
+		life = 20;
+		score = 10;
 	}
 
 	public void move() {
@@ -11,22 +21,17 @@ public class EnemyBase extends Enemy {
 		if (x < 100)
 			vx = 1;
 		if (Math.random() < 0.05) {
-			GameWorld.enemies.add(new StraightEnemy(x, y, 0, 2));
+			GameWorld.enemies.add(new StraightEnemy(x, y, 0, 1));
 		}
 		if (Math.random() < 0.05) {
-			GameWorld.enemies.add(new DropEnemy(x, y, 0, 1));
-			GameWorld.enemies.add(new CurveEnemy(x, y, 0, 1));
 			GameWorld.enemies.add(new RandomEnemy(x, y, 0, 1));
-			GameWorld.enemies.add(new StraightEnemy(x, y, 0, 1));
-			
 		}
-	
+		if (Math.random() < 0.02) {
+			GameWorld.enemies.add(new DropEnemy(x, y, 0, 1));
+		}
+		if (Math.random() < 0.02) {
+			GameWorld.enemies.add(new CurveEnemy(x, y, 0, 1             ));
+		}
+
 	}
-	public void draw(MyFrame f) {
-		f.setColor(0, 128, 0);
-		f.fillOval(x, y, 32, 32);
-		f.setColor(200, 200, 200);
-		f.fillOval(x - 16, y + 8, 64, 16);
-	}
-	
 }
--- src/GameFrame.java
+++ src/GameFrame.java
@@ -5,25 +5,68 @@ public class GameFrame extends MyFrame {
 		GameWorld.player = new Player(100, 300, 0, 0);
 		addKeyListener(GameWorld.player);
 		GameWorld.playerBullets = new Vector<PlayerBullet>();
-		GameWorld.enemies=new Vector<Enemy>();
-		GameWorld.enemies.add(new EnemyBase(100,50,1,0));
+		GameWorld.enemies = new Vector<Enemy>();
+		GameWorld.enemies.add(new EnemyBase(100, 50, 1, 0));
 		while (true) {
 			clear();
 			GameWorld.player.draw(this);
 			GameWorld.player.move();
 			movePlayerBullets();
 			moveEnemies();
-			for(int i=0; i<GameWorld.enemies.size();i++){
-				Enemy e=GameWorld.enemies.get(i);
-				e.draw(this);
-				e.move();
-			}
-			sleep(0.03);
+			checkPlayerAndEnemies();
+			checkPlayerBulletsAndEnemies();
+			
+			sleep(0.01);
 		}
 	}
+
+	public void checkPlayerBulletsAndEnemies() {
+		int i = 0;
+		while (i < GameWorld.playerBullets.size()) {
+
+			PlayerBullet b = GameWorld.playerBullets.get(i);
+
+			int j = 0;
+			int hits = 0;
+
+			while (j < GameWorld.enemies.size()) {
+
+				Enemy e = GameWorld.enemies.get(j);
+				if (checkhit(e, b)) {
+					System.out.println("あたり");
+					hits++;
+					e.life--;
+				}
+				if (e.life <= 0) {
+					GameWorld.enemies.remove(j);
+				} else {
+					j++;
+				}
+			}
+
+			if (hits > 0) {
+				GameWorld.playerBullets.remove(i);
+			} else {
+				i++;
+			}
+
+		}
+	}
+
+	public void checkPlayerAndEnemies() {
+		for (int i = 0; i < GameWorld.enemies.size(); i++) {
+			Enemy e = GameWorld.enemies.get(i);
+			if (checkhit(GameWorld.player, e)) {
+				System.out.println("やられた");
+				GameWorld.player.y = -1000;
+			}
+
+		}
+	}
+
 	public void moveEnemies() {
-		for (int i=0; i<GameWorld.enemies.size();i++) {
-			Enemy e=GameWorld.enemies.get(i);
+		for (int i = 0; i < GameWorld.enemies.size(); i++) {
+			Enemy e = GameWorld.enemies.get(i);
 			e.draw(this);
 			e.move();
 		}
@@ -46,4 +89,12 @@ public class GameFrame extends MyFrame {
 
 	}
 
+	public boolean checkhit(Character a, Character b) {
+		if (Math.abs(a.x - b.x) <= 30 && Math.abs(a.y - b.y) <= 30) {
+			return true;
+		} else {
+			return false;
+
+		}
+	}
 }
